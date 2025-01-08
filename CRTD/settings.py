a80d2f55a50bf458783cd8962790abebc360872b
SEBIN BABY <sebin.18@gmail.com>
2025-01-08 14:06:11 +0530

    db

--- CRTD/settings.py
+++ CRTD/settings.py
@@ -81,10 +81,9 @@ WSGI_APPLICATION = 'CRTD.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-
-
-
-DATABASES = {
+selected_db = 0
+if selected_db:
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
             'NAME': config('DATABASE_NAME'),
@@ -94,7 +93,13 @@ DATABASES = {
             'PORT':config('DATABASE_PORT'),
         }
 }
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',  # or use a path from config like: config('SQLITE_DB_PATH')
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
