0550eb7cb540969c2831d4479ea8e707a92cfef7
Sebin Baby <sebin.18@gmail.com>
2025-01-08 06:51:05 +0000

    db

--- CRTD/settings.py
+++ CRTD/settings.py
@@ -83,20 +83,8 @@ WSGI_APPLICATION = 'CRTD.wsgi.application'
 
 
 
-selected_db = 0
-if selected_db:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': config('DATABASE_NAME'),
-            'USER': config('DATABASE_USER'),
-            'PASSWORD': config('DATABASE_PASSWORD'),
-            'HOST': config('DATABASE_HOST'),
-            'PORT':config('DATABASE_PORT'),
-        }
-}
-else:
-    DATABASES = {
+
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',  # or use a path from config like: config('SQLITE_DB_PATH')
