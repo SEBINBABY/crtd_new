ff3b89b7472899b42b2eb84056bbba8a3b0911d7
SEBIN BABY <sebin.18@gmail.com>
2025-01-15 16:06:25 +0530

    settings local

--- CRTD/settings.py
+++ CRTD/settings.py
@@ -83,10 +83,9 @@ WSGI_APPLICATION = 'CRTD.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-
-
-
-DATABASES = {
+selected_db = 0
+if selected_db:
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
             'NAME': config('DATABASE_NAME'),
@@ -96,6 +95,13 @@ DATABASES = {
             'PORT':config('DATABASE_PORT'),
         }
 }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',  # or use a path from config like: config('SQLITE_DB_PATH')
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -133,24 +139,21 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# Directory where all static files will be collected
-STATIC_ROOT = '/home/ubuntu/crtd_new/staticfiles/'
+# # Directory where all static files will be collected
+# STATIC_ROOT = '/home/ubuntu/crtd_new/staticfiles/'
 
-# Include additional static file directories
-STATICFILES_DIRS = [
-    '/home/ubuntu/crtd_new/admin_dashboard/static/',
-    '/home/ubuntu/crtd_new/payment_integration/static/',
-    '/home/ubuntu/crtd_new/quiz/static/',
-    '/home/ubuntu/crtd_new/users/static/'
-]
+# # Include additional static file directories
+# STATICFILES_DIRS = [
+#     '/home/ubuntu/crtd_new/admin_dashboard/static/',
+#     '/home/ubuntu/crtd_new/payment_integration/static/',
+#     '/home/ubuntu/crtd_new/quiz/static/',
+#     '/home/ubuntu/crtd_new/users/static/'
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 SITE_ID = 1
-CSRF_TRUSTED_ORIGINS = [
-    'https://test.crtd.in',
-]
 
 # Send_email settings
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
