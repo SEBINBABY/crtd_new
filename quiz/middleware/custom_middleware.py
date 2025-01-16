d53178125b0b2baa07f59e031ca88a75804a7ade
Sebin Baby <sebin.18@gmail.com>
2025-01-16 09:29:30 +0000

    disqualify fix

--- quiz/middleware/custom_middleware.py
+++ quiz/middleware/custom_middleware.py
@@ -1,14 +1,14 @@
-from django.shortcuts import redirect
-from django.utils.deprecation import MiddlewareMixin
+# from django.shortcuts import redirect
+# from django.utils.deprecation import MiddlewareMixin
 
-class DisqualificationMiddleware(MiddlewareMixin):
-    def process_request(self, request):
-        # Only track requests for the test page
-        if "/quiz/" in request.path or "/quiz_summary" in request.path:
-            referer = request.META.get("HTTP_REFERER", "")
-            allowed_domain = request.get_host()  # Your site's domain
-            if not referer or allowed_domain not in referer:
-                print("=============Custom Middleware Disqualification Occured=============")
-                print("host: ",request.get_host())
-                print("referer: ",referer)
-                return redirect("quiz:disqualify")
+# class DisqualificationMiddleware(MiddlewareMixin):
+#     def process_request(self, request):
+#         # Only track requests for the test page
+#         if "/quiz/" in request.path or "/quiz_summary" in request.path:
+#             referer = request.META.get("HTTP_REFERER", "")
+#             allowed_domain = request.get_host()  # Your site's domain
+#             if not referer or allowed_domain not in referer:
+#                 print("=============Custom Middleware Disqualification Occured=============")
+#                 print("host: ",request.get_host())
+#                 print("referer: ",referer)
+#                 return redirect("quiz:disqualify")
